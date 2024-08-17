d4a0022ccccb5f32bd2c117d95c54cc61d2784ef
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2024-08-17 15:53:47 +0530

    Time: 622 ms (11.25%) | Memory: 45 MB (35.90%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,40 +1,38 @@
 class Solution {
     public String shortestPalindrome(String s) {
-       if(isPal(s)){
-        return s;
-       } 
-       StringBuilder sb=new StringBuilder(s);
-       int e=s.length();
-       while(e!=0){
-        String str=sb.substring(0,e);
-           if(!isPal(str)){
-            e--;
-           }
-           else{
-            break;
-           }
-       }
-       String ad=sb.substring(e,sb.length());
-       String rev="";
-       for(int i=ad.length()-1;i>=0;i--){
-        String ss=Character.toString(ad.charAt(i));
-        rev=rev+ss;
-       }
-     StringBuilder ns=new StringBuilder(rev);
-     ns.append(sb);
-      return ns.toString();
-       
+        if (isPal(s)) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder(s);
+        int e = s.length();
+        while (e != 0) {
+            String str = sb.substring(0, e);
+            if (!isPal(str)) {
+                e--;
+            } else {
+                break;
+            }
+        }
+        String ad = sb.substring(e, sb.length());
+        String rev = "";
+        for (int i = ad.length() - 1; i >= 0; i--) {
+            String ss = Character.toString(ad.charAt(i));
+            rev = rev + ss;
+        }
+        StringBuilder ns = new StringBuilder(rev);
+        ns.append(sb);
+        return ns.toString();
     }
-    public boolean isPal(String s){
-        int st=0;
-        int  e=s.length()-1;
-        
-        while(st<=e){
-            if(s.charAt(e)!=s.charAt(st)){
+
+    public boolean isPal(String s) {
+        int st = 0;
+        int e = s.length() - 1;
+        while (st <= e) {
+            if (s.charAt(e) != s.charAt(st)) {
                 return false;
             }
-        st++;
-        e--;
+            st++;
+            e--;
         }
         return true;
     }
