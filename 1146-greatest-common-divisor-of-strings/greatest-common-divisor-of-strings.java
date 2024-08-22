83eaea48d085ed4ecdd822a8367302a3b37c402e
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2024-08-22 19:28:02 +0530

    Time: 1 ms (70.86%) | Memory: 42.6 MB (16.04%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,55 +1,23 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-       if(str1.indexOf(str2)==-1 && str2.indexOf(str1)==-1 ){
-        return "";
-       }
-       int n1=str1.length();
-       int n2=str2.length();
-       
-       String ss="";
-      for(int i=n2-1;i>=0;i--){
-        String cs=str2.substring(i,n2);
-        String ms="";
-        for(int j=0;j<n1;j++){
-           ms=ms+cs;
-           if(ms.equals(str1)){
-           ss=cs;
-            break;
-           }
+        if ((str1 + str2).equals(str2 + str1)) {
+            int a = str1.length();
+            int b = str2.length();
+            int gcd = gcd(a, b);
+            return str1.substring(0, gcd);
+        } else {
+            return "";
         }
-        if(ms.equals(str1)){
-            break;
-        }
-      }
-    //   if(hm.size()==0){
-    //   return "";
-    //   }
-    if(ss.length()==0){
-        return "";
     }
-      StringBuilder sb1=new StringBuilder(str1);
-      StringBuilder sb2=new StringBuilder(str2);
-      int c1=0;
-      int c2=0;
-      while(sb1.length()!=0){
-        sb1.delete(0,ss.length());
-        c1++;
-      }
-      while(sb2.length()!=0){
-        sb2.delete(0,ss.length());
-        c2++;
-      }
-      int gcd=0;
-       for(int i=Math.min(c1,c2);i>=1;i--){
-        if(c1%i==0 && c2%i==0){
-           gcd=i;
-           break;
+
+    public int gcd(int a, int b) {
+        int ans=0;
+        for (int i = Math.min(a,b); i >= 1; i--) {
+            if (a % i == 0 && b % i == 0) {
+                ans=i;
+                break;
+            }
         }
-       }
-       String str="";
-       for(int i=0;i<gcd;i++){
-        str=str+ss;
-       }
-      return str;
+        return ans;
     }
 }
