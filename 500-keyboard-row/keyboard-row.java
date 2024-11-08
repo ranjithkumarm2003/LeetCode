1660bfe5f675d201021b27300b8275dbbb820cf6
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2024-11-08 17:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (87.71%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String alpha[]={"QWERTYUIOPqwertyuiop","ASDFGHJKLasdfghjkl","ZXCVBNMzxcvbnm"};
+        ArrayList<String>li=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            char ch=words[i].charAt(0);
+            int alpind=-1;
+            for(int j=0;j<3;j++){
+                if(alpha[j].indexOf(ch)!=-1){
+                   alpind=j;
+                    break;
+                }
+            }
+            int c=0;
+            for(int j=0;j<words[i].length();j++){
+                if(alpha[alpind].indexOf(words[i].charAt(j))!=-1){
+                    c++;
+                }
+            }
+            if(c==words[i].length()){
+               li.add(words[i]);
+            }
+        }
+        String ans[]=new String[li.size()];
+        int m=0;
+        for(String i:li){
+            ans[m]=i;
+            m++;
+        }
+        return ans;
+    }
+}
