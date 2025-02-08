ed0178944492c305a0a1bc8c7c72429ea5c5e213
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-02-08 11:42:26 +0530

    Time: 1883 ms (5.03%) | Memory: 40.5 MB (40.41%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        for(int i=1;i<=num/2;i++){
+            if(i*i==num){
+                return true;
+            }
+            if(i*i > num){
+                return false;
+            }
+        }
+        return false;
+    }
+}
