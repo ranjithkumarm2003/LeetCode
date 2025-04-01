48060b295c30952f5ea6aa9daa16f862cda794fe
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-01 11:09:22 +0530

    Time: 3 ms (21.98%) | Memory: 42.1 MB (94.13%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,63 +1,24 @@
 class Solution {
-    public int[] decrypt(int[] code, int k) {
-        if(k==0){
-             return new int[code.length];
-        }
-        else if(k>0){
-            return pos(code,k);
-          
-        }
-        else{
-           return neg(code,k);
-        
-        }
 
-      
-    }
-    public int[] pos(int code[],int k){
-       int arr[]=new int[code.length];
-       for(int i=0;i<code.length;i++){
-        int sum=0;int ind=0;
-        int j=0;
-           if(i==code.length-1){
-            j=0;
-           }
-           else{
-            j=i+1;
-           }
-          for( ;j<code.length;j++){
-            sum+=code[j];
-            ind++;
-            if(ind==k)break;
-            if(j==code.length-1){
-               j=-1;
+    public int[] decrypt(int[] code, int k) {
+        int[] result = new int[code.length];
+        // If k is 0, return the result directly.
+        if (k == 0) {
+            return result;
+        }
+        for (int i = 0; i < result.length; i++) {
+            if (k > 0) {
+                // If k is greater than 0, store the sum of next k numbers.
+                for (int j = i + 1; j < i + k + 1; j++) {
+                    result[i] += code[j % code.length];
+                }
+            } else {
+                // If k is less than 0, store the sum of previous -1*k numbers.
+                for (int j = i - Math.abs(k); j < i; j++) {
+                    result[i] += code[(j + code.length) % code.length];
+                }
             }
-          }
-          arr[i]=sum;
-       }
-       return arr;
-    }
-    public int[] neg(int code[],int k){
-        int arr[]=new int[code.length];
-       for(int i=0;i<code.length;i++){
-        int sum=0;int ind=0;
-        int j=0;
-           if(i==0){
-            j=code.length-1;;
-           }
-           else{
-            j=i-1;
-           }
-          for( ;j>=0;j--){
-            sum+=code[j];
-            ind--;
-            if(ind==k)break;
-            if(j==0){
-               j=code.length;
-            }
-          }
-          arr[i]=sum;
-       }
-       return arr;
+        }
+        return result;
     }
 }
