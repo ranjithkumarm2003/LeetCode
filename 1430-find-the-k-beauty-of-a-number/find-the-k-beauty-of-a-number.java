697cd7fac43373f56eaec233525a0344eaef8a19
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-04 13:19:54 +0530

    Time: 2 ms (10.28%) | Memory: 40.5 MB (59.73%) - LeetSync

--- 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.java
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.java
@@ -4,7 +4,7 @@ class Solution {
         String s=""+num;
         for(int i=0;i<=s.length()-k;i++){
             try{
-            if(num% Integer.parseInt(s.substring(i,i+k))==0) c++;
+            if(num% Integer.valueOf(s.substring(i,i+k))==0) c++;
             }
             catch(ArithmeticException e){
                 continue;
