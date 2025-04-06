ab5486b855efedf94c82c397a91f65942e545bd0
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-06 11:46:48 +0530

    Time: 2 ms (96.70%) | Memory: 44 MB (54.60%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
@@ -1,67 +1,19 @@
+
 class Solution {
     public double minimumAverage(int[] nums) {
-        int n=nums.length;
-        double ans[]=new double[n/2];
-        int i=0,j=n-1;
-        int max=0;
-        int min=51;
-        int min_ind=-1;
-        int max_ind=-1;
-        int k=0;
-        while(true){
-           
-            if(nums[i]<nums[j]){
-                if(nums[i]==0 && i<j){
-                i++;
-                continue;
-            }
-                if(nums[i]<min && nums[i]!=0 ){
-                    min=nums[i];
-                    min_ind=i;
-                }
-            }
-            else{
-                if(nums[j]<min && nums[j]!=0){
-                    min=nums[j];
-                    min_ind=j;
-                }
-            }
-            if(nums[i]>nums[j] ){
-                 if(nums[j]==0 && j>i){
-                j--;
-                continue;
-            }
-                if(nums[i]>max){
-                    max=nums[i];
-                    max_ind=i;
-                }
-            }
-            else{
-                if(nums[j]>max && nums[j]!=0){
-                    max=nums[j];
-                    max_ind=j;
-                }
-            }
-            if(i<=j) i++;
-               
-           if(j>=i) j--;
-            
-            
-            if(i>j){
-                  i=0;
-                  j=n-1;
-                  nums[min_ind]=0;
-                  nums[max_ind]=0;
-                  ans[k++]=(double)(min+max)/2;
-                  min=51;
-                  max=0;
-                  if(k==n/2) break;
-            }
-           
+        Arrays.sort(nums); 
 
+        int left = 0;
+        int right = nums.length - 1;
+        double minAvg = Double.MAX_VALUE;
+
+        while (left < right) {
+            double avg = (nums[left] + nums[right]) / 2.0;
+            minAvg = Math.min(minAvg, avg);
+            left++;
+            right--;
         }
-        Arrays.sort(ans);
-        return ans[0];
 
+        return minAvg;
     }
 }
