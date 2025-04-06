a4436efa841f1e37ee8ed7b405ced0605fb5f564
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-06 12:29:13 +0530

    Time: 2 ms (74.23%) | Memory: 43.4 MB (41.24%) - LeetSync

--- 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.java
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.java
@@ -1,26 +1,43 @@
 class Solution {
     public int incremovableSubarrayCount(int[] nums) {
-        int ans = 0;
         int n = nums.length;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+
+        // Step 1: Compute prefix increasing
+        pre[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pre[i] = (nums[i] > nums[i - 1]) ? pre[i - 1] + 1 : 1;
+        }
+
+        // Step 2: Compute suffix increasing
+        suf[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = (nums[i] < nums[i + 1]) ? suf[i + 1] + 1 : 1;
+        }
+
+        int count = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                boolean ok = true;
-                int lst = -1;
+                boolean isValid = true;
 
-                for (int k = 0; k < n; k++) {
-                    if (k >= i && k <= j) {
-                        continue;
-                    } else {
-                        ok &= (lst < nums[k]);
-                        lst = nums[k];
+                if (i > 0 && j < n - 1) {
+                    if (nums[i - 1] >= nums[j + 1]) {
+                        isValid = false;
                     }
                 }
 
-                ans += ok ? 1 : 0;
+                if (isValid) {
+                    if (i == 0 || pre[i - 1] == i) {
+                        if (j == n - 1 || suf[j + 1] == n - j - 1) {
+                            count++;
+                        }
+                    }
+                }
             }
         }
-        return ans;
+
+        return count;
     }
 }
-
