fe1dfe8cfcc6c147533396d66c56b7d806bd3fd4
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-14 21:37:54 +0530

    Time: 1 ms (99.82%) | Memory: 61.8 MB (41.21%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -7,7 +7,7 @@ class Solution {
            if(buyStock>prices[i]){
             buyStock=prices[i];
            }
-           if(prices[i]-buyStock>max){
+           else if(prices[i]-buyStock>max){
             max=prices[i]-buyStock;
            }
             
