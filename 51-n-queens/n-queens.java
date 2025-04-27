54bddfb31eaa8bfea8ec590f0860dbd12549d9ec
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-04-27 13:17:08 +0530

    Time: 13 ms (6.27%) | Memory: 44.8 MB (88.77%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,125 +1,74 @@
-import java.util.*;
 class Solution {
-    public static boolean isSafe(char board[][],int row,int col)
-    {
-        int n=board.length;
-        //check vertically-->row chnages but col fixed
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result=new ArrayList<>();
+        
+        char[][] board=new char[n][n];
+
         for(int i=0;i<n;i++)
         {
-            if(board[i][col]=='Q')
+            for(int j=0;j<n;j++)
             {
-                return false;
+                board[i][j]='.';
             }
         }
+        solve(result,board,0,n);
+        return result;
+    }
+    public void solve( List<List<String>> result, char[][]board,int col,int n)
+    {
+        if(col==n)
+        {
+            List<String>list=new ArrayList<>();
+            for(int i=0;i<n;i++)
+            {
+                String str="";
+                for(int j=0;j<n;j++)
+                {
+                    str=str+board[i][j];
+                }
+                list.add(str);
+            }
+            result.add(list);
+            return;
+        }
+        else
+        {
+            for(int row=0;row<n;row++)
+            {
+                if(isSafe(row,col,board,n)==true)
+                {
+                    board[row][col]='Q';
+                    solve(result,board,col+1,n);
+                    board[row][col]='.';
+                }
+            }
+        }
+    }
 
-        //check horizontally-->col changes but row same 
-        for(int j=0;j<n;j++)
+    public boolean isSafe(int row,int col,char[][]board,int n)
+    {
+        int i,j;
+        for(j=col-1;j>=0;j--)
         {
             if(board[row][j]=='Q')
             {
                 return false;
             }
         }
-
-        //Now its time to check for all 4 Diagonals
-
-        //2 Right Diagonals as R1 and R2
-        //R1 = top right
-        int i=row;
-        int j=col;
-        while(i>=0 && j<n)
+        for(i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
         {
             if(board[i][j]=='Q')
             {
                 return false;
             }
-            i--;
-            j++;
         }
-        //R2 - bottom right
-        i=row;
-        j=col;
-        while(i<n && j<n)
+        for(i=row+1,j=col-1;i<n&&j>=0;i++,j--)
         {
             if(board[i][j]=='Q')
             {
                 return false;
             }
-            i++;
-            j++;
-        }
-
-        //2 Left Diagonals as L1 and L2
-        //L1 = top left
-        i=row;
-        j=col;
-        while(i>=0 && j>=0)
-        {
-            if(board[i][j]=='Q')
-            {
-                return false;
-            }
-            i--;
-            j--;
-        }
-
-        //L2 = bottom left
-        i=row;
-        j=col;
-        while(i<n && j>=0)
-        {
-            if(board[i][j]=='Q')
-            {
-                return false;
-            }
-            i++;
-            j--;
         }
         return true;
-    }
-    public static void board(char arr[][],int row,List<List<String>> mainlist)
-    {
-        if(row==arr.length)
-        {
-            //make a list and add in the mainlist
-            List<String> sublist=new ArrayList<>();
-            String str="";
-            for(int i=0;i<arr.length;i++)
-            {
-                for(int j=0;j<arr.length;j++)
-                {
-                    str+=arr[i][j];
-                }
-                sublist.add(str);
-                str="";
-            }
-            mainlist.add(sublist);
-            return;
-        }
-        for(int j=0;j<arr.length;j++)
-        {
-            if(isSafe(arr,row,j))
-            {
-                arr[row][j]='Q';
-                board(arr,row+1,mainlist);
-                arr[row][j]='.';//backtracking
-            }
-        }
-    }
-    public List<List<String>> solveNQueens(int n) {
-        //all possible solutions will be there in a single mainList
-        //each sublist will be a string type
-
-        List<List<String>> mainlist=new ArrayList<>();
-        char arr[][]=new char[n][n];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                arr[i][j]='.';
-            }
-        }
-        board(arr,0,mainlist);
-        return mainlist;
     }
 }
