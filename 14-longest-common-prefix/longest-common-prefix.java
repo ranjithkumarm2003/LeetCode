534df00868bbc8a2d1bf04648ffec05b7bbb9821
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-05-02 18:50:59 +0530

    Time: 7 ms (12.75%) | Memory: 42.3 MB (10.46%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,7 +3,7 @@ class Solution {
        // StringBuilder sb=new StringBuilder();
        String ans="";
         Arrays.sort(strs);
-        System.out.print(Arrays.toString(strs));
+        // System.out.print(Arrays.toString(strs));
         String str1=strs[0];
         String str2=strs[strs.length-1];
         for(int i=0;i<Math.min(str1.length(),str2.length());i++){
