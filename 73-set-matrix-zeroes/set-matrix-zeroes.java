e2ebd6bfab65587b17e673ebe52319aeca1bd67a
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-05-04 11:31:20 +0530

    Time: 12 ms (4.71%) | Memory: 45.9 MB (16.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,30 +2,30 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
+        Set<String>set=new HashSet<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][j]=Integer.MAX_VALUE-5;
+                    String s=""+i+","+j;
+                    set.add(s);
                 }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==Integer.MAX_VALUE-5){
+                if(set.contains(""+i+","+j)){
                     int row=0;
                     int col=0;
                     while(row<n || col<m){
                         if(row<n){
-                            if(matrix[i][row]!=Integer.MAX_VALUE-5)
                                   matrix[i][row]=0;
                            row++;
                         }
                         if(col<m){
-                            if(matrix[col][j]!=Integer.MAX_VALUE-5)
                                   matrix[col][j]=0;
                         col++;
                         }
-                      matrix[i][j]=0;
+                      
                     }
 
                 }
