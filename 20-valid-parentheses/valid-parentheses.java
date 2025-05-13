0a6fa3a28f23e635b2dbc390e36a8e9db9b59794
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-05-13 20:46:59 +0530

    Time: 2 ms (97.25%) | Memory: 42.2 MB (8.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -7,15 +7,18 @@ class Solution {
             if (c == '[' || c == '{' || c == '(') {
                 st.push(c);
             } 
-            else if(st.isEmpty()) return false;
             else {
                 if (c == ']') {
+                    if(st.isEmpty()) return false;
                     char d = st.pop();
                     if (d != '[')return false;
                 } else if (c == '}') {
+                    //System.out.println(st.toString());
+                    if(st.isEmpty()) return false;
                     char d = st.pop();
                     if (d != '{') return false;
                 } else {
+                    if(st.isEmpty()) return false;
                     char d = st.pop();
                     if (d != '(')return false;
                 }
