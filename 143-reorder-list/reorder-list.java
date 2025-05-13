1d7f4eeef9875c070409217dbce98a7ea874cdb2
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-05-13 19:20:58 +0530

    Time: 1 ms (99.81%) | Memory: 48.4 MB (33.69%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,25 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
-        ListNode fir = head;
-        ListNode sec = head.next;
-        while (sec.next != null) {
-            ListNode pre = head;
-            ListNode cur = head.next;
-            while (cur.next != null) {
-                pre = pre.next;
-                cur = cur.next;
-            }
-            fir.next = cur;
-            cur.next = sec;
-            pre.next = null;
-            fir = fir.next.next;
-            sec=fir.next;
-            if (fir.next== null) {
-                break;
-            }
+
+public void reorderList(ListNode head) {
+    if (head == null || head.next == null || head.next.next == null)
+        return;
+    reorder(head, head.next.next);
+}
+
+ListNode reorder(ListNode curr, ListNode fast) {
+    if (fast == null) {
+        ListNode toMove = curr.next;
+        curr.next = null;
+        return toMove;
+    }
+    // 1 2 3 4
+    // 1,3 2,null -> 4
+
+    // 1,3 2,5 3,null -> 4
+    // 1 5 2 4 3
+    ListNode toInsert;
+    if (fast.next == null) {
+        toInsert = reorder(curr.next, null);
+    } else {
+        if (fast.next.next == null) {
+            toInsert = reorderEven(curr.next);
+        } else {
+            toInsert = reorder(curr.next, fast.next.next);
         }
     }
+    ListNode toMove = toInsert.next;
+    ListNode next = curr.next;
+    curr.next = toInsert;
+    toInsert.next = next;
+    return toMove;
 }
+
+private ListNode reorderEven(ListNode node) {
+    ListNode toMove = node.next.next;
+    node.next.next = null;
+    return toMove;
+}}
