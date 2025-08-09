b6d6ed9bc89b93a24b71619a23bcd297a340f4dd
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-08-09 10:13:13 +0530

    Time: 13 ms (32.12%) | Memory: 45.2 MB (37.74%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -10,17 +10,14 @@ class Solution {
                 magMap.put(c,magMap.get(c)+1);
             }
         }
-        int n=magMap.size();
-        int count=0;
+        
         for(char c: ransomNote.toCharArray()){
-            if(!magMap.containsKey(c)){
+            if(!magMap.containsKey(c) || magMap.get(c)<=0){
                 return false;
                 //magMap.put(c,1)
             }
-            else{
-                magMap.put(c,magMap.get(c)-1);
-                if(magMap.get(c)==-1) return false;
-            }
+             magMap.put(c,magMap.get(c)-1);
+                
         }
         return true;
     }
