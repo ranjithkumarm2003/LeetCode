41d22da173c1fc045ebafd974896ecb069c2eb33
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-08-15 18:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (88.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,46 +1,51 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs==null || strs.length==0 || strs[0].length()==0) return "";
-        String ans="";
-        int n=strs.length;
-        int k=0;
-        while(true){
-            boolean flag=true;
-            for(int j=1;j<n;j++){
-               if(!(k<strs[j].length()) || !(k<strs[0].length())){
-                flag=false;
-                break;
-               }
-               if(!(strs[0].charAt(k)==strs[j].charAt(k))){
-                   flag=false;
-                break; 
+        if (strs == null || strs.length == 0) return "";
+
+        String pref = strs[0];
+        int prefLen = pref.length();
+
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+                prefLen--;
+                if (prefLen == 0) {
+                    return "";
                 }
-            } 
-            if(flag && k<strs[0].length()){
-                ans+= strs[0].charAt(k++);         
-            }
-            else{
-                break;
+                pref = pref.substring(0, prefLen);
             }
         }
-        return ans;
+
+        return pref;
     }
 }
 
-// if (strs == null || strs.length == 0) return "";
+ 
 
-//         String pref = strs[0];
-//         int prefLen = pref.length();
 
-//         for (int i = 1; i < strs.length; i++) {
-//             String s = strs[i];
-//             while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
-//                 prefLen--;
-//                 if (prefLen == 0) {
-//                     return "";
+// runs in 8 ms 
+
+// if(strs==null || strs.length==0 || strs[0].length()==0) return "";
+//         String ans="";
+//         int n=strs.length;
+//         int k=0;
+//         while(true){
+//             boolean flag=true;
+//             for(int j=1;j<n;j++){
+//                if(!(k<strs[j].length()) || !(k<strs[0].length())){
+//                 flag=false;
+//                 break;
+//                }
+//                if(!(strs[0].charAt(k)==strs[j].charAt(k))){
+//                    flag=false;
+//                 break; 
 //                 }
-//                 pref = pref.substring(0, prefLen);
+//             } 
+//             if(flag && k<strs[0].length()){
+//                 ans+= strs[0].charAt(k++);         
+//             }
+//             else{
+//                 break;
 //             }
 //         }
-
-//         return pref; 
+//         return ans;
