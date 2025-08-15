534db5cc4de35617c42a0e6dae27610d618143bb
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-08-15 17:20:51 +0530

    Time: 8 ms (8.28%) | Memory: 42.2 MB (9.70%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,21 +1,45 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        
-        String pref = strs[0];
-        int prefLen = pref.length();
-
-        for (int i = 1; i < strs.length; i++) {
-            String s = strs[i];
-            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
-                prefLen--;
-                if (prefLen == 0) {
-                    return "";
+        String ans="";
+        int n=strs.length;
+        int k=0;
+        for(int i=0;i<strs[0].length();i++){
+            boolean flag=true;
+            for(int j=1;j<n;j++){
+               if(!(k<strs[j].length()) || !(k<strs[0].length())){
+                flag=false;
+                break;
+               }
+               if(!(strs[0].charAt(k)==strs[j].charAt(k))){
+                   flag=false;
+                break; 
                 }
-                pref = pref.substring(0, prefLen);
+            } 
+            if(flag){
+                ans+= strs[0].charAt(k++);         
+            }
+            else{
+                break;
             }
         }
-
-        return pref;        
+        return ans;
     }
 }
+
+// if (strs == null || strs.length == 0) return "";
+
+//         String pref = strs[0];
+//         int prefLen = pref.length();
+
+//         for (int i = 1; i < strs.length; i++) {
+//             String s = strs[i];
+//             while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+//                 prefLen--;
+//                 if (prefLen == 0) {
+//                     return "";
+//                 }
+//                 pref = pref.substring(0, prefLen);
+//             }
+//         }
+
+//         return pref; 
