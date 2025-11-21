338b60483c40ee35f9fd154c49990d91e023d3c4
ranjithkumarm2003 <146807929+ranjithkumarm2003@users.noreply.github.com>
2025-11-21 16:14:35 +0530

    Time: 47 ms (14.52%) | Memory: 90.7 MB (18.12%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -3,6 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int diff=0;
         for(int i=0;i<nums.length-1;i++){
+           
            diff=Math.max(Math.abs(nums[i]-nums[i+1]),diff);
         }
         return diff;
